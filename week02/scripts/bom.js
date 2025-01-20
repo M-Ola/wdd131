0c75a5a4269be6562b81865bc6c9268b7906b22b
ola466@gmail.com <ola466@gmail.com>
2025-01-19 19:15:18 -0600

    completed temple album

--- week02/scripts/bom.js
+++ week02/scripts/bom.js
@@ -25,7 +25,7 @@ button.addEventListener('click', function () {
    
          deleteButton.addEventListener('click', function () {
              list.removeChild(li);
-             input.focus();
+            
 
 
 
---
+++ week02/scripts/temples.js
@@ -0,0 +1,16 @@
+const hamButton = document.querySelector('#menu');
+const navigation = document.querySelector('.navigation');
+
+hamButton.addEventListener('click', () => {
+    navigation.classList.toggle('open');
+    hamButton.classList.toggle('open');
+});
+
+
+
+document.getElementById('currentyear').innerHTML = new Date().getFullYear()
+
+
+
+
+document.getElementById('lastmodified').innerHTML += new Date(document.lastModified).toLocaleString()
