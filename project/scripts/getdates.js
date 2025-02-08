31cde8723f40380d9f4e8bf4974a31243190e6be
ola466@gmail.com <ola466@gmail.com>
2025-02-07 21:13:35 -0600

    completed site plan

---
+++ project/scripts/getdates.js
@@ -0,0 +1,7 @@
+
+document.getElementById('currentyear').innerHTML = new Date().getFullYear()
+
+
+
+
+document.getElementById('lastmodified').innerHTML += new Date(document.lastModified).toLocaleString()
